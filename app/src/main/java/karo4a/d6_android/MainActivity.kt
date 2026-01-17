fd3393658098a59d7f84fcb24a01060bda449adf
Karo4a <t1mati666@yandex.ru>
2026-01-17 14:31:42 +0300

    almost done

--- app/src/main/java/karo4a/d6_android/MainActivity.kt
+++ app/src/main/java/karo4a/d6_android/MainActivity.kt
@@ -1,12 +1,19 @@
 package karo4a.d6_android
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rollButton: Button
+    private lateinit var rollText : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        rollText = findViewById(R.id.roll_textView)
+        rollButton = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { clickRollButton() }
+    }
+
+    private fun clickRollButton() {
+        rollText.text = Random.Default.nextInt(1..6).toString()
     }
 }
